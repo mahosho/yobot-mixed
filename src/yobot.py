a5cf136b65e5d8778da2e155ead021595de014b2
yuudi <26199752+yuudi@users.noreply.github.com>
2019-10-01 18:12:16 +0800

    Update and rename yobot.py to src/yobot.py

--- src/yobot.py
+++ src/yobot.py
@@ -8,34 +8,34 @@ from reserve import Reserve
 from yobot_msg import Message
 
 
-def yobot(cmd_list):
+def yobot(*cmd_list):
     txt_list = []
-    if len(cmd_list) != 5:
+    if len(cmd_list) != 4:
         txt_list.append("100参数错误")
     else:
-        func = Message.match(cmd_list[4])
+        func = Message.match(cmd_list[3])
         if func != 0:
             txt_list.append(Message.msg(func))
             return txt_list  # 后面不再运行
-        func = Lock.match(cmd_list[4])
+        func = Lock.match(cmd_list[3])
         if func != 0:
-            lockboss = Lock(cmd_list[1:4])
-            lockboss.lockboss(cmd_list[4], func)
+            lockboss = Lock(cmd_list[:3])
+            lockboss.lockboss(cmd_list[3], func)
             txt_list.extend(lockboss.txt_list)
             return txt_list  # 后面不再运行
-        func = Record.match(cmd_list[4])
+        func = Record.match(cmd_list[3])
         if func != 0:
-            report = Record(cmd_list[1:4])
-            report.rep(cmd_list[4], func)
+            report = Record(cmd_list[:3])
+            report.rep(cmd_list[3], func)
             txt_list.extend(report.txt_list)
             if func == 3 or func == 4:
                 pass  # 后面可能继续运行
             else:
                 return txt_list  # 后面不再运行
-        func = Reserve.match(cmd_list[4])
+        func = Reserve.match(cmd_list[3])
         if func != 0:
-            rsv = Reserve(cmd_list[1:4])
-            rsv.rsv(cmd_list[4], func)
+            rsv = Reserve(cmd_list1:3])
+            rsv.rsv(cmd_list[3], func)
             txt_list.extend(rsv.txt_list)
             return txt_list  # 后面不再运行
     if txt_list == []:
@@ -44,5 +44,5 @@ def yobot(cmd_list):
 
 
 if __name__ == "__main__":
-    txtlist = yobot(sys.argv)  # 获得输出文本的list
+    txtlist = yobot(*sys.argv[1:4])  # 获得输出文本的list
     print("\n".join(txtlist))  # 随便怎么用，这里直接连接并输出
