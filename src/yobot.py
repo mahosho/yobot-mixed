362073d4a4edd6ac1fc71c542ed83b7629ac22a0
26199752+yuudi@users.noreply.github.com <26199752+yuudi@users.noreply.github.com>
2019-10-03 15:09:48 +0800

    update ver info

--- src/yobot.py
+++ src/yobot.py
@@ -15,7 +15,7 @@ def yobot(*cmd_list):
         txt_list.append("100参数错误")
     else:
         if cmd_list[3] == "ver":
-            txt_list.append("yobot [ver 2.0.5.2]")
+            txt_list.append("yobot [ver 2.0.6.0]")
             return txt_list  # 后面不再运行
         u = Check()
         if cmd_list[3] == "更新":
