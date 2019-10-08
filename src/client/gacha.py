13d4b826ab9a376ef66a68607cfb393908a12af1
26199752+yuudi@users.noreply.github.com <26199752+yuudi@users.noreply.github.com>
2019-10-08 19:55:17 +0800

    add result function

--- src/client/gacha.py
+++ src/client/gacha.py
@@ -1,9 +1,10 @@
 # coding=utf-8
-import json5
 import os.path
+import random
 import sqlite3
 import sys
 
+import json5
 import requests
 
 
@@ -43,10 +44,25 @@ class Gacha():
                     return 2
         return 0
 
+    def result(self):
+        prop = 0.
+        result_list = []
+        for p in self.__pool["pool"].values():
+            prop += p["prop"]
+        resu = random.random() * prop
+        for _ in range(10):
+            resu_i = resu
+            for p in self.__pool["pool"].values():
+                resu_i -= p["prop"]
+                if resu_i < 0:
+                    result_list.append(p["prefix"]+random.choice(p["pool"]))
+                    break
+        return result_list
+
     def gacha(self):
         db_exists = os.path.exists(os.path.join(self.__path, "collections.db"))
         db_conn = sqlite3.connect(os.path.join(self.__path, "collections.db"))
-        db=db_conn.cursor()
+        db = db_conn.cursor()
         if not db_exists:
             db.execute(
                 '''CREAT TABLE Colle(
