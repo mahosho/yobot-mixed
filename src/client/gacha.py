c5c9e1ad84abd315c7587c7b94b670c47b9d16d7
26199752+yuudi@users.noreply.github.com <26199752+yuudi@users.noreply.github.com>
2019-10-08 17:38:15 +0800

    init

--- src/client/gacha.py
+++ src/client/gacha.py
@@ -1,7 +1,35 @@
-#coding=utf-8
+# coding=utf-8
+import json
+import os.path
+import sqlite3
+import sys
+
+import requests
+
 
 class Gacha():
     URL = "http://api.yobot.xyz/v2/pool/?type=json"
 
-    def __init__(self):
+    def __init__(self, baseinfo):
+        """
+        baseinfo=[群号，QQ号, 群名片]（字符串）
+        """
+        self.__groupid = baseinfo[0]
+        self.__qqid = baseinfo[1]
+        self.__nickname = baseinfo[2]
+        self.__path = os.path.dirname(sys.argv[0])
+        self.txt_list = []
+        if os.path.exists(os.path.join(self.__path, "pool.json")):
+            with open(os.path.join(self.__path, "pool.json"), "r", encoding="utf-8") as f:
+                self.__data = json.load(f)
+        else:
+            res = requests.get(URL)
+            assert res.status_code == 200, "服务器不可用"
+            try:
+                self.__data = json.loads(res.text)
+            except:
+                print("服务器错误")
+                exit()
+
+    def __del__(self):
         pass
